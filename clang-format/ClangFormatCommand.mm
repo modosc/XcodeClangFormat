852e5ce5dd86b0ec2448dc13f3f9e2140ca04b4b
arnaudbrejeon <arnaud@tangibleplay.com>
2016-09-20 18:18:45 -0700

    More stable selections and cursor update

--- clang-format/ClangFormatCommand.mm
+++ clang-format/ClangFormatCommand.mm
@@ -1,25 +1,8 @@
 #import "ClangFormatCommand.h"
 
-#import <AppKit/AppKit.h>
-
 #include <clang/Format/Format.h>
 
-// Generates a list of offsets for ever line in the array.
-void updateOffsets(std::vector<size_t>& offsets, NSMutableArray<NSString*>* lines) {
-    offsets.clear();
-    offsets.reserve(lines.count + 1);
-    offsets.push_back(0);
-    size_t offset = 0;
-    for (NSString* line in lines) {
-        offsets.push_back(offset += line.length);
-    }
-}
-
-NSErrorDomain errorDomain = @"ClangFormatError";
-
 @implementation ClangFormatCommand
-
-NSUserDefaults* defaults = nil;
 
 - (NSData*)getCustomStyle {
     // First, read the regular bookmark because it could've been changed by the wrapper app.
@@ -33,43 +16,41 @@ NSUserDefaults* defaults = nil;
                                    bookmarkDataIsStale:&regularStale
                                                  error:nil];
     }
-
+    
     if (!regularURL) {
         return nil;
     }
-
+    
     // Then read the security URL, which is the URL we're actually going to use to access the file.
     NSData* securityBookmark = [defaults dataForKey:@"securityBookmark"];
     NSURL* securityURL = nil;
     BOOL securityStale = NO;
     if (securityBookmark) {
-        securityURL = [NSURL URLByResolvingBookmarkData:securityBookmark
-                                                options:NSURLBookmarkResolutionWithSecurityScope |
-                                                        NSURLBookmarkResolutionWithoutUI
-                                          relativeToURL:nil
-                                    bookmarkDataIsStale:&securityStale
-                                                  error:nil];
+        securityURL = [NSURL
+                       URLByResolvingBookmarkData:securityBookmark
+                       options:NSURLBookmarkResolutionWithSecurityScope | NSURLBookmarkResolutionWithoutUI
+                       relativeToURL:nil
+                       bookmarkDataIsStale:&securityStale
+                       error:nil];
     }
-
+    
     // Clear out the security URL if it's no longer matching the regular URL.
-    if (securityStale == YES ||
-        (securityURL && ![[securityURL path] isEqualToString:[regularURL path]])) {
+    if (securityStale == YES || (securityURL && ![[securityURL path] isEqualToString:[regularURL path]])) {
         securityURL = nil;
     }
-
+    
     if (!securityURL && regularStale == NO) {
         // Attempt to create new security URL from the regular URL to persist across system reboots.
         NSError* error = nil;
-        securityBookmark = [regularURL
-                   bookmarkDataWithOptions:NSURLBookmarkCreationWithSecurityScope |
-                                           NSURLBookmarkCreationSecurityScopeAllowOnlyReadAccess
-            includingResourceValuesForKeys:nil
-                             relativeToURL:nil
-                                     error:&error];
+        securityBookmark = [regularURL bookmarkDataWithOptions:NSURLBookmarkCreationWithSecurityScope |
+                            NSURLBookmarkCreationSecurityScopeAllowOnlyReadAccess
+                                includingResourceValuesForKeys:nil
+                                                 relativeToURL:nil
+                                                         error:&error];
         [defaults setObject:securityBookmark forKey:@"securityBookmark"];
         securityURL = regularURL;
     }
-
+    
     if (securityURL) {
         // Finally, attempt to read the .clang-format file
         NSError* error = nil;
@@ -82,8 +63,63 @@ NSUserDefaults* defaults = nil;
             return data;
         }
     }
-
+    
     return nil;
+}
+
+std::vector<clang::tooling::Range> computeOffsets(NSMutableArray<NSString*>* lines) {
+    size_t offset = 0;
+    auto output = std::vector<clang::tooling::Range>();
+    output.reserve([lines count]);
+    for (NSString* line in lines) {
+        output.emplace_back(offset, offset + line.length);
+        offset += line.length;
+    }
+    
+    return output;
+}
+
+size_t lineFromOffset(const std::vector<clang::tooling::Range>& offsets, unsigned offset) {
+    const auto it = std::find_if(offsets.cbegin(), offsets.cend(), [offset](const clang::tooling::Range& lineOffset) {
+        return offset >= lineOffset.getOffset() && offset < lineOffset.getOffset() + lineOffset.getLength();
+    });
+    return std::distance(offsets.cbegin(), it);
+}
+
+size_t columnFromOffset(const std::vector<clang::tooling::Range>& offsets, unsigned offset) {
+    const auto line = lineFromOffset(offsets, offset);
+    return offset - offsets[line].getOffset();
+}
+
+NSErrorDomain errorDomain = @"ClangFormatError";
+NSUserDefaults* defaults = nil;
+
+- (BOOL)setFormatWithStyle:(NSString*)style format:(clang::format::FormatStyle*)format {
+    format->Language = clang::format::FormatStyle::LK_Cpp;
+    clang::format::getPredefinedStyle("LLVM", format->Language, format);
+    
+    if ([style isEqualToString:@"custom"]) {
+        NSData* config = [self getCustomStyle];
+        if (!config) {
+            return NO;
+        } else {
+            // parse style
+            llvm::StringRef configString(reinterpret_cast<const char*>(config.bytes), config.length);
+            auto error = clang::format::parseConfiguration(configString, format);
+            if (error) {
+                return NO;
+            }
+        }
+    } else {
+        auto success =
+        clang::format::getPredefinedStyle(llvm::StringRef([style cStringUsingEncoding:NSUTF8StringEncoding]),
+                                          clang::format::FormatStyle::LanguageKind::LK_Cpp, format);
+        if (!success) {
+            return NO;
+        }
+    }
+    
+    return YES;
 }
 
 - (void)performCommandWithInvocation:(XCSourceEditorCommandInvocation*)invocation
@@ -91,141 +127,88 @@ NSUserDefaults* defaults = nil;
     if (!defaults) {
         defaults = [[NSUserDefaults alloc] initWithSuiteName:@"XcodeClangFormat"];
     }
-
+    
     NSString* style = [defaults stringForKey:@"style"];
     if (!style) {
         style = @"llvm";
     }
-
+    
     clang::format::FormatStyle format = clang::format::getLLVMStyle();
-    format.Language = clang::format::FormatStyle::LK_Cpp;
-    clang::format::getPredefinedStyle("LLVM", format.Language, &format);
-    if ([style isEqualToString:@"custom"]) {
-        NSData* config = [self getCustomStyle];
-        if (!config) {
-            completionHandler([NSError
-                errorWithDomain:errorDomain
-                           code:0
-                       userInfo:@{
-                           NSLocalizedDescriptionKey :
-                               @"Could not load custom style. Please open XcodeClangFormat.app"
-                       }]);
-        } else {
-            // parse style
-            llvm::StringRef configString(reinterpret_cast<const char*>(config.bytes),
-                                         config.length);
-            auto error = clang::format::parseConfiguration(configString, &format);
-            if (error) {
-                completionHandler([NSError
-                    errorWithDomain:errorDomain
-                               code:0
-                           userInfo:@{
-                               NSLocalizedDescriptionKey :
-                                   [NSString stringWithFormat:@"Could not parse custom style: %s.",
-                                                              error.message().c_str()]
-                           }]);
-                return;
-            }
-        }
-    } else {
-        auto success = clang::format::getPredefinedStyle(
-            llvm::StringRef([style cStringUsingEncoding:NSUTF8StringEncoding]),
-            clang::format::FormatStyle::LanguageKind::LK_Cpp, &format);
-        if (!success) {
-            completionHandler([NSError
-                errorWithDomain:errorDomain
-                           code:0
-                       userInfo:@{
-                           NSLocalizedDescriptionKey : [NSString
-                               stringWithFormat:@"Could not parse default style %@", style]
-                       }]);
-            return;
-        }
-    }
-
-    NSData* buffer = [invocation.buffer.completeBuffer dataUsingEncoding:NSUTF8StringEncoding];
-    llvm::StringRef code(reinterpret_cast<const char*>(buffer.bytes), buffer.length);
-
-    std::vector<size_t> offsets;
-    updateOffsets(offsets, invocation.buffer.lines);
-
-    std::vector<clang::tooling::Range> ranges;
-    for (XCSourceTextRange* range in invocation.buffer.selections) {
-        const size_t start = offsets[range.start.line] + range.start.column;
-        const size_t end = offsets[range.end.line] + range.end.column;
-        ranges.emplace_back(start, end - start);
-    }
-
-    // Calculated replacements and apply them to the input buffer.
-    const llvm::StringRef filename("<stdin>");
-    auto replaces = clang::format::reformat(format, code, ranges, filename);
-    auto result = clang::tooling::applyAllReplacements(code, replaces);
-
-    if (!result) {
-        // We could not apply the calculated replacements.
-        completionHandler([NSError
-            errorWithDomain:errorDomain
-                       code:0
-                   userInfo:@{
-                       NSLocalizedDescriptionKey : @"Failed to apply formatting replacements."
-                   }]);
+    const BOOL success = [self setFormatWithStyle:style format:&format];
+    if (success != YES) {
+        completionHandler([NSError errorWithDomain:errorDomain
+                                              code:0
+                                          userInfo:@{
+                                                     NSLocalizedDescriptionKey : [NSString stringWithFormat:@"Could not set style: %@", style]
+                                                     }]);
         return;
     }
-
-    // Remove all selections before replacing the completeBuffer, otherwise we get crashes when
-    // changing the buffer contents because it tries to automatically update the selections, which
-    // might be out of range now.
+    
+    // Retrieve buffer and its offsets
+    auto code = llvm::StringRef([invocation.buffer.completeBuffer UTF8String]);
+    auto offsets = computeOffsets(invocation.buffer.lines);
+    
+    // Force range to full buffer
+    auto ranges = std::vector<clang::tooling::Range>();
+    ranges.emplace_back(0, code.size());
+    
+    auto replaces = clang::format::reformat(format, code, ranges);
+    if (replaces.empty()) {
+        completionHandler([NSError errorWithDomain:errorDomain
+                                              code:0
+                                          userInfo:@{
+                                                     NSLocalizedDescriptionKey : [NSString stringWithFormat:@"Style %@ already OK", style]
+                                                     }]);
+        return;
+    }
+    
+    auto result = clang::tooling::applyAllReplacements(code, replaces);
+    if (!result) {
+        // We could not apply the calculated replacements.
+        completionHandler([NSError errorWithDomain:errorDomain
+                                              code:0
+                                          userInfo:@{
+                                                     NSLocalizedDescriptionKey : @"Failed to apply formatting replacements."
+                                                     }]);
+        return;
+    }
+    
+    // Copy selections as they probably be destroyed when setting the buffer
+    NSArray<XCSourceTextRange*>* selections =
+    [[NSArray alloc] initWithArray:invocation.buffer.selections copyItems:YES];
     [invocation.buffer.selections removeAllObjects];
-
-    // Update the entire text with the result we got after applying the replacements.
-    invocation.buffer.completeBuffer = [[NSString alloc] initWithBytes:result->data()
-                                                                length:result->size()
-                                                              encoding:NSUTF8StringEncoding];
-
-    // Recalculate the line offsets.
-    updateOffsets(offsets, invocation.buffer.lines);
-
-    // Update the selections with the shifted code positions.
-    for (auto& range : ranges) {
-        const size_t start = clang::tooling::shiftedCodePosition(replaces, range.getOffset());
-        const size_t end =
-            clang::tooling::shiftedCodePosition(replaces, range.getOffset() + range.getLength());
-
-        // In offsets, find the value that is smaller than start.
-        auto start_it = std::lower_bound(offsets.begin(), offsets.end(), start);
-        auto end_it = std::lower_bound(offsets.begin(), offsets.end(), end);
-        if (start_it == offsets.end() || end_it == offsets.end()) {
-            continue;
-        }
-
-        // We need to go one line back unless we're at the beginning of the line.
-        if (*start_it > start) {
-            --start_it;
-        }
-        if (*end_it > end) {
-            --end_it;
-        }
-
-        const size_t start_line = std::distance(offsets.begin(), start_it);
-        const int64_t start_column = int64_t(start) - int64_t(*start_it);
-
-        const size_t end_line = std::distance(offsets.begin(), end_it);
-        const int64_t end_column = int64_t(end) - int64_t(*end_it);
-
+    
+    invocation.buffer.completeBuffer =
+    [[NSString alloc] initWithBytes:result->data() length:result->length() encoding:NSUTF8StringEncoding];
+    
+    const auto offsetsNew = computeOffsets(invocation.buffer.lines);
+    
+    // Re-create selections
+    for (XCSourceTextRange* range in selections) {
+        auto start = offsets[range.start.line].getOffset() + (int) range.start.column;
+        auto end = offsets[range.end.line].getOffset() + (int) range.end.column;
+        
+        start = clang::tooling::shiftedCodePosition(replaces, start);
+        end = clang::tooling::shiftedCodePosition(replaces, end);
+        
+        const auto start_line = lineFromOffset(offsetsNew, start);
+        const auto start_column = columnFromOffset(offsetsNew, start);
+        const auto end_line = lineFromOffset(offsetsNew, end);
+        const auto end_column = columnFromOffset(offsetsNew, end);
+        
         [invocation.buffer.selections
-            addObject:[[XCSourceTextRange alloc]
-                          initWithStart:XCSourceTextPositionMake(start_line, start_column)
-                                    end:XCSourceTextPositionMake(end_line, end_column)]];
+         addObject:[[XCSourceTextRange alloc] initWithStart:XCSourceTextPositionMake(start_line, start_column)
+                                                        end:XCSourceTextPositionMake(end_line, end_column)]];
     }
-
-    // If we could not recover the selection, place the cursor at the beginning of the file.
-    if (!invocation.buffer.selections.count) {
+    
+    // If we could not recover any selection, place the cursor at the beginning of the file.
+    if (invocation.buffer.selections.count == 0) {
         [invocation.buffer.selections
-            addObject:[[XCSourceTextRange alloc] initWithStart:XCSourceTextPositionMake(0, 0)
-                                                           end:XCSourceTextPositionMake(0, 0)]];
+         addObject:[[XCSourceTextRange alloc] initWithStart:XCSourceTextPositionMake(0, 0)
+                                                        end:XCSourceTextPositionMake(0, 0)]];
     }
-
+    
     completionHandler(nil);
+    return;
 }
-
 @end
